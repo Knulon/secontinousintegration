fa67637485e96a2a969b97a27a7583745841e079
Marlon Hörner <marlonhorner@M1Prolon.fritz.box>
2023-06-30 17:30:50 +0200

    primetests

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -23,13 +23,21 @@ public class PrimeCheckTest {
   @Test
   public void testPrimes() {
     /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    assertEquals("Test für 3", true, PrimeCheck.isPrime(3));
+    assertEquals("Test für 5", true, PrimeCheck.isPrime(5));
+    assertEquals("Test für 7", true, PrimeCheck.isPrime(7));
+    assertEquals("Test für 19", true, PrimeCheck.isPrime(19));
+    assertEquals("Test für 23", true, PrimeCheck.isPrime(23));
   }
-
+  
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
     /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    assertEquals("Test für 4", false, PrimeCheck.isPrime(4));
+    assertEquals("Test für 6", false, PrimeCheck.isPrime(6));
+    assertEquals("Test für 9", false, PrimeCheck.isPrime(9));
+    assertEquals("Test für 12", false, PrimeCheck.isPrime(12));
+    assertEquals("Test für 36", false, PrimeCheck.isPrime(36));
   }
 }
